a5c9c47ca1f14d0524b26f880597f041709b62fd
Andrez5643 <AndrezArellano77@gmail.com>
2025-05-05 11:15:39 -0500

    Update gameon_bot.py

--- gameon_bot.py
+++ gameon_bot.py
@@ -12,10 +12,10 @@ bot = telebot.TeleBot(BOT_TOKEN, parse_mode="Markdown")
 
 # Setup Google Sheets
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds_path = os.path.join(os.path.dirname(__file__), "credentials.json")
-creds = ServiceAccountCredentials.from_json_keyfile_name(creds_path, scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name("/app/credentials.json", scope)
 client = gspread.authorize(creds)
 SHEET_NAME = "Game On Player Ledger"
+
 
 # Log to sheet
 def log_transaction_to_sheet(telegram_handle, first_name, sportsbook_username, password, action, amount, method, status):
